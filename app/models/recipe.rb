a5cbb8cb48462330110ca06ade9b3084fdace533
OscarCA8 <oicarrasco@miuandes.cl>
2025-10-09 19:00:22 -0300

    authentication implementation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+    belongs_to :user
     has_rich_text :instructions
     validates :title, presence: true
     validates :cook_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
