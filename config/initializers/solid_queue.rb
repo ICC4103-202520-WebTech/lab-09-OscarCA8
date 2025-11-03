d36fae6c64126b2146e74a7c4d7b0f08402fd514
OscarCA8 <oicarrasco@miuandes.cl>
2025-11-03 18:42:39 -0300

    Remove problematic solid_queue initializer

--- config/initializers/solid_queue.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.after_initialize do
-  if defined?(SolidQueue::Record)
-    SolidQueue::Record.connects_to(
-      writing: ActiveRecord::Base.configurations.configs_for(env_name: Rails.env).find { |c| c.name == "queue" }.name.to_sym
-    )
-  end
-end
